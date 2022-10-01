7ed5f61832cb90002103a13295a6f97a63575c89
Alan Santos <ss.alan@outlook.com>
2022-10-01 20:05:20 -0300

    html and css for nav component

--- sustentaculo-front/sustentaculo/src/app/app-routing.module.ts
+++ sustentaculo-front/sustentaculo/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { NavComponent } from "./components/nav/nav.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: NavComponent,
   },
 ];
