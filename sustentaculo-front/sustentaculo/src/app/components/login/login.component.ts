a4f899f0ba7dea7f99fdcdeca971aaad4a30a5ae
Alan Santos <ss.alan@outlook.com>
2022-10-03 11:08:31 -0300

    toastr and code

--- sustentaculo-front/sustentaculo/src/app/components/login/login.component.ts
+++ sustentaculo-front/sustentaculo/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
 
 @Component({
@@ -16,6 +17,14 @@ export class LoginComponent implements OnInit {
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
 
+  constructor(private toast: ToastrService) {}
+
+  ngOnInit(): void {}
+
+  logar() {
+    this.toast.error('Usuário e/ou senha inválidos!', 'Login');
+  }
+
   validaCampos(): boolean {
     if (this.email.valid && this.senha.valid) {
       return true;
@@ -23,8 +32,4 @@ export class LoginComponent implements OnInit {
       return false;
     }
   }
-
-  constructor() {}
-
-  ngOnInit(): void {}
 }
